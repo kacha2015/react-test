14bc63e3dccca98de3545f227c6010ed5498d0df
kacha2015 <romittimarcelo@gmail.com>
2021-08-25 09:16:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-91rdj1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-91rdj1)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitzhfdshh!</h1>
+      <p>Start editing to see some magic hapgtgpen :)</p>
+    </div>
+  );
+}
